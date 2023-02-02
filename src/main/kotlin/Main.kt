ab569c24bd83968f0529bafb6f9a483b384626e0
lekishonsheila <lekishonsheila@gmail.com>
2023-02-02 21:57:18 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+  fun main (){
+      var name = "lekishonsheila"
+       println(name)
+        var age: Byte =15
+      println(15)
+      var phonenumber: Long= +27645787960
+      println(+27645787960)
+      var weight: Short= 55
+      println(55)
+      var citizenshipIsKenyan: Boolean= false
+      println(citizenshipIsKenyan)
+
+
+
+
+}
